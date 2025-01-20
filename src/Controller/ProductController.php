3b5160069d209216f87add9097060fb9b021e097
fabien1981 <166442156+fabien1981@users.noreply.github.com>
2025-01-20 21:45:17 +0100

    Ajout de la fonctionnalité d'ajout de produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Categories;
 use App\Entity\Products;
+use App\Form\CategoriesType;
 use App\Form\ProductType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,20 +15,45 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductController extends AbstractController
 {
     
-    #[Route('/product', name: 'app_product')]
-    public function index(Request $request, EntityManagerInterface $entity): Response
+    #[Route('/product/add', name: 'app_productadd')]
+    public function ajout(Request $request, EntityManagerInterface $entity): Response
     {
         $product = new Products();
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
+
         if($form->isSubmitted() && $form->isValid()){
             $entity->persist($product);
             $entity->flush();
             $this->addFlash("message", "Produit ajouté avec succés");
             return $this->redirectToRoute("app_product");
         }
-        return $this->render('product/index.html.twig', [
+
+        /**
+         * Formulaire pour catégories
+         */
+        $category = new Categories();
+        $formCategorie = $this->createForm(CategoriesType::class, $category);
+        $formCategorie->handleRequest($request);
+        if($formCategorie->isSubmitted() && $formCategorie->isValid()){
+            $entity->persist($category);
+            $entity->flush();
+            $this->addFlash("message", "Catégorie ajoutée avec succés");
+            return $this->redirectToRoute('app_productadd');
+        }
+        return $this->render('product/add.html.twig', [
             'form' => $form->createView(),
+            'formCategorie'=>$formCategorie,
+        ]);
+    }
+
+    #[Route('/product', name: 'app_product')]
+    public function index(Request $request, EntityManagerInterface $entity): Response
+    {
+        $product = $entity->getRepository(Products::class)->findAll();
+       
+        return $this->render('product/index.html.twig', [
+            'products'=>$product,
         ]);
     }
 }
